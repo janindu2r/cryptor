679f92e2df7eff895a7f89e3e72d52714b39c9e8
Janindu Ranawake <janindu93@gmail.com>
2016-02-22 23:23:25 +0530

    Enum for algorithm selection

--- Cryptor/CryptoEngine.cs
+++ Cryptor/CryptoEngine.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace Cryptor
 {
-    class CryptoEngine
+    public class CryptoEngine
     {
+        SetAlgoritham AlgorithmID;
+        bool bWithKey = false;
+        string keyword = "";
+        public byte[] Vector;
+
+        public CryptoEngine(SetAlgoritham AlgoID, string key) 
+        {
+            if (key.Length == 0)
+                bWithKey = false;
+            else
+                bWithKey = true;
+
+            keyword = key;
+            AlgorithmID = AlgoID;
+        }
+
+    }
+
+    public enum SetAlgoritham 
+    {
+        DES = 0, RC2, Rijndael, TripleDes
     }
 }
